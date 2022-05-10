380cc50285d7d9e3620a1ab8deb1bf6becac545c
photowey <photowey@gmail.com>
2022-05-11 01:54:45 +0800

    feat: add config loader

---
+++ pkg/loader/loader.go
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2022 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package loader
+
+import (
+	`fmt`
+
+	"github.com/spf13/viper"
+)
+
+// Load a func load config form fs by viper[github](https://github.com/spf13/viper).
+//
+// @param fileName the target config file's name
+//
+// @param fileType the target config file's type
+//
+// @param dst the target struct of decode
+//
+// @param paths search paths
+//
+/*
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/appname/")  // path to look for the config file in
+	viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
+	viper.AddConfigPath(".")              // optionally look for config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+...
+*/
+func Load(fileName, fileType string, dst any, pathList ...string) error {
+	viper.SetConfigName(fileName)
+	viper.SetConfigType(fileType)
+	viper.SetEnvPrefix("author")
+	for _, path := range pathList {
+		viper.AddConfigPath(path)
+	}
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("Fatal error config file: %w \n", err)
+	}
+
+	err = viper.Unmarshal(dst)
+	if err != nil {
+		return fmt.Errorf("unable to decode into struct, %v", err)
+	}
+
+	return nil
+}
---
+++ pkg/loader/loader_test.go
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2022 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package loader_test
+
+import (
+	`testing`
+
+	`github.com/photowey/nettygo/pkg/loader`
+)
+
+type (
+	config struct {
+		Author
+	}
+	Author struct {
+		Name  string
+		Email string
+	}
+)
+
+func TestLoad(t *testing.T) {
+	type args struct {
+		fileName string
+		fileType string
+		dst      any
+		pathList []string
+	}
+	var dst config
+
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test load config file",
+			args: args{
+				fileName: "config",
+				fileType: "yml",
+				dst:      &dst,
+				pathList: []string{"./testdata/"},
+			},
+			want:    "photowey",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := loader.Load(tt.args.fileName, tt.args.fileType, tt.args.dst, tt.args.pathList...); (err != nil) != tt.wantErr {
+				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if dst.Name != tt.want {
+				t.Errorf("Load() error got = %v, want = %v", dst.Name, tt.want)
+			}
+			t.Log("ok")
+		})
+	}
+}
