6b21cfc2ff01acdbd2e25e820c7797d0ab66b3c2
photowey <photowey@gmail.com>
2022-05-22 01:48:43 +0800

    feat: 1.add byteunit

--- interal/byteunit/byteunit.go
+++ interal/byteunit/byteunit.go
@@ -14,11 +14,15 @@
  * limitations under the License.
  */
 
-package buf
+package byteunit
 
 const (
 	Byte = 1
-	KB   = 1024 * Byte
-	MB   = 1024 * KB
-	GB   = 1024 * MB
+	K    = 1024
+	KB   = K * Byte
+	MB   = K * KB
+	GB   = K * MB
+	TB   = K * GB
+	PB   = K * TB
+	EB   = K * PB
 )
